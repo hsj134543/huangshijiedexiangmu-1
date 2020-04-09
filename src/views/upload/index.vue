ca0790b7951e8eb025e001329443b8b31a03b07d
guahunyo <2736859895@qq.com>
2020-04-09 19:58:06 +0800

    上传文件并转化，优化接口

--- src/views/upload/index.vue
+++ src/views/upload/index.vue
@@ -1,17 +1,38 @@
 <template>
   <div class="app-container">
     <el-upload
+      drag
       action="http://47.105.182.216:2500/index/upload/upload"
       :headers="headers"
       :on-preview="handlePreview"
       :on-remove="handleRemove"
+      :on-success="handleSuccess"
       :limit="1"
+      accept=".zip"
       :before-remove="beforeRemove"
       :on-exceed="handleExceed"
       :file-list="fileList"
     >
-      <el-button size="small" type="primary">点击上传</el-button>
-      <div slot="tip" class="el-upload__tip">只能上传zip文件</div>
+      <i class="el-icon-upload" />
+      <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+      <div slot="tip" class="el-upload__tip">只能上传zip文件，上传文件并转化</div>
+    </el-upload>
+    <el-upload
+      drag
+      action="http://47.105.182.216:2500/index/upload/optimize"
+      :headers="headers"
+      :on-preview="handlePreview"
+      :on-remove="handleRemove"
+      :on-success="handleSuccess"
+      :limit="1"
+      accept=".zip"
+      :before-remove="beforeRemove"
+      :on-exceed="handleExceed"
+      :file-list="fileList"
+    >
+      <i class="el-icon-upload" />
+      <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+      <div slot="tip" class="el-upload__tip">只能上传zip文件,上传文件并优化</div>
     </el-upload>
   </div>
 </template>
@@ -30,16 +51,26 @@ export default {
   },
   methods: {
     handleRemove(file, fileList) {
-      console.log(file, fileList)
     },
+
     handlePreview(file) {
-      console.log(file)
     },
+
     handleExceed(files, fileList) {
       this.$message.warning(`当前限制选择 1 个文件`)
     },
+
     beforeRemove(file, fileList) {
       return this.$confirm(`确定移除 ${file.name}？`)
+    },
+
+    handleSuccess(res, file, fileList) {
+      if (res.code) {
+        this.$message.success(res.msg)
+      } else {
+        this.$message.error(res.msg)
+      }
+      this.fileList = []
     }
   }
 }
